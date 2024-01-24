ba1959174cb13284e6ba5c0046edbd2b5749e4d8
David Conroy <dconroy@gmail.com>
2024-01-24 17:44:16 -0500

    latest not working

--- python/processors/add_stake/processor.py
+++ python/processors/add_stake/processor.py
@@ -8,6 +8,7 @@ from utils.models.schema_names import ADD_STAKE_SCHEMA_NAME
 from utils.session import Session
 from utils.processor_name import ProcessorName
 import json
+import logging
 from datetime import datetime
 from time import perf_counter
 
@@ -57,8 +58,7 @@ class AddStakeProcessor(TransactionsProcessor):
                 else:
                     print("we have a match")
 
-
-                print(event)
+                logging.warning(event.type_str)
 
                 creation_number = event.key.creation_number
                 sequence_number = event.sequence_number
@@ -66,65 +66,65 @@ class AddStakeProcessor(TransactionsProcessor):
                 # account_address = general_utils.standardize_address(
                 #     event.key.account_address
                 # )
-        #         pool_address = general_utils.standardize_address(
-        #              event.pool_address                 )
-        #         # delegator_address = general_utils.standardize_address(
-        #         #     event.key.delegator_address
-        #         # )
-        #         # amount_added = int(data["amount_added"])
-        #         # add_stake_fee = int(data["add_stake_fee"])
+                pool_address = general_utils.standardize_address(
+                     event.key.pool_address
+                 )
+                # delegator_address = general_utils.standardize_address(
+                #     event.key.delegator_address
+                # )
+                # amount_added = int(data["amount_added"])
+                # add_stake_fee = int(data["add_stake_fee"])
                
 
-        #         # Convert your on-chain data scheme to database-friendly values
-        #         # Our on-chain struct looks like this:
-        #         #   struct CoinFlipEvent has copy, drop, store {
-        #         #       prediction: bool,
-        #         #       result: bool,
-        #         #       timestamp: u64,
-        #         #   }
-        #         # These values are stored in the `data` field of the event as JSON fields/values
-        #         # Load the data into a json object and then use it as a regular dictionary
-        #         data = json.loads(event.data)
+                # Convert your on-chain data scheme to database-friendly values
+                # Our on-chain struct looks like this:
+                #   struct CoinFlipEvent has copy, drop, store {
+                #       prediction: bool,
+                #       result: bool,
+                #       timestamp: u64,
+                #   }
+                # These values are stored in the `data` field of the event as JSON fields/values
+                # Load the data into a json object and then use it as a regular dictionary
+                data = json.loads(event.data)
 
-        #         # prediction = bool(data["prediction"])
-        #         # result = bool(data["result"])
-        #         # wins = int(data["wins"])
-        #         # losses = int(data["losses"])
+                # prediction = bool(data["prediction"])
+                # result = bool(data["result"])
+                # wins = int(data["wins"])
+                # losses = int(data["losses"])
 
-        #         # We have extra data to insert into the database, because we want to process our data.
-        #         # Calculate the total
-        #         # win_percentage = wins / (wins + losses)
+                # We have extra data to insert into the database, because we want to process our data.
+                # Calculate the total
+                # win_percentage = wins / (wins + losses)
 
-        #         # Create an instance of AddStakeEvent
-        #         event_db_obj = AddStakeEvent(
-        #             sequence_number=sequence_number,
-        #             creation_number=creation_number,
-        #             pool_address=pool_address,
-        #             # delegator_address=delegator_address,
-        #             # amount_added=amount_added,
-        #             # add_stake_fee=add_stake_fee,   
-        #             transaction_version=transaction_version,
-        #             transaction_timestamp=transaction_timestamp,
-        #             # losses=losses,
-        #             # prediction=prediction,
-        #             # result=result,
-        #             # wins=wins,
-        #             # win_percentage=win_percentage,
-        #             event_index=event_index,  # when multiple events of the same type are emitted in a single transaction, this is the index of the event in the transaction
-        #         )
-        #         event_db_objs.append(event_db_obj)
+                # Create an instance of AddStakeEvent
+                event_db_obj = AddStakeEvent(
+                    sequence_number=sequence_number,
+                    creation_number=creation_number,
+                    pool_address=pool_address,
+                    # delegator_address=delegator_address,
+                    # amount_added=amount_added,
+                    # add_stake_fee=add_stake_fee,   
+                    transaction_version=transaction_version,
+                    transaction_timestamp=transaction_timestamp,
+                    # losses=losses,
+                    # prediction=prediction,
+                    # result=result,
+                    # wins=wins,
+                    # win_percentage=win_percentage,
+                    event_index=event_index,  # when multiple events of the same type are emitted in a single transaction, this is the index of the event in the transaction
+                )
+                event_db_objs.append(event_db_obj)
 
-        # processing_duration_in_secs = perf_counter() - start_time
-        # start_time = perf_counter()
-        # self.insert_to_db(event_db_objs)
-        # db_insertion_duration_in_secs = perf_counter() - start_time
-        # return ProcessingResult(
-        #     start_version=start_version,
-        #     end_version=end_version,
-        #     processing_duration_in_secs=processing_duration_in_secs,
-        #     db_insertion_duration_in_secs=db_insertion_duration_in_secs,
-        # )
-        
+        processing_duration_in_secs = perf_counter() - start_time
+        start_time = perf_counter()
+        self.insert_to_db(event_db_objs)
+        db_insertion_duration_in_secs = perf_counter() - start_time
+        return ProcessingResult(
+            start_version=start_version,
+            end_version=end_version,
+            processing_duration_in_secs=processing_duration_in_secs,
+            db_insertion_duration_in_secs=db_insertion_duration_in_secs,
+        )
 
     def insert_to_db(self, parsed_objs: List[AddStakeEvent]) -> None:
         with Session() as session, session.begin():
